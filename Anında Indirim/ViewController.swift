661fd95a74ae0841b943d3d4c87325bc42d003ab
caginakgul <cagin@letsbecool.com>
2016-12-13 17:48:23 +0300

    Initial Commit

---
+++ Anında Indirim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Anında Indirim
+//
+//  Created by cagin akgul on 13.12.2016.
+//  Copyright © 2016 cagin akgul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
